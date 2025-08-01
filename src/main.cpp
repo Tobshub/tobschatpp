ae76389d9b3307d7061b0ff8e43fbb3424bb564f
Tobani E-G <tobanigeorge@gmail.com>
2025-08-01 05:40:06 +0100

    Configure formatter

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,7 @@ enum RoomPermission {
 };
 
 map<string, RoomPermission> string_perm_map{
-    {"owner", Owner},   {"admin", Admin}, {"chat", Chat},
-    {"notify", Notify}, {"none", None},
+    {"owner", Owner}, {"admin", Admin}, {"chat", Chat}, {"notify", Notify}, {"none", None},
 };
 
 RoomPermission permission_from_string(string perm) {
@@ -46,8 +45,7 @@ public:
   Room *room;
   map<uuid, Room *> invites;
   map<uuid, Room *> rooms;
-  explicit Context(const WebSocketChannelPtr &channel, Room *room,
-                   bool is_active = true)
+  explicit Context(const WebSocketChannelPtr &channel, Room *room, bool is_active = true)
       : channel(channel), room(room), is_active(is_active) {
     ACTIVE_CONTEXT.insert_or_assign(channel->id(), this);
   }
@@ -55,8 +53,7 @@ public:
     if (this->is_active)
       ACTIVE_CONTEXT.erase(channel->id());
   };
-  static Context *build(const WebSocketChannelPtr &channel,
-                        Room *default_room) {
+  static Context *build(const WebSocketChannelPtr &channel, Room *default_room) {
     auto it = ACTIVE_CONTEXT.find(channel->id());
     if (it != ACTIVE_CONTEXT.end()) {
       return it->second;
@@ -70,9 +67,7 @@ public:
   void join(Room *room, RoomPermission);
   void leave(Room *room);
 
-  string nickOrId() {
-    return this->nickname.empty() ? to_string(this->id()) : this->nickname;
-  }
+  string nickOrId() { return this->nickname.empty() ? to_string(this->id()) : this->nickname; }
 
   void send(string message) {
     if (this->channel->isClosed()) {
@@ -113,19 +108,15 @@ public:
 
   void join(RoomMember member) {
     this->members.insert_or_assign(member.ctx->id(), member);
-    this->broadcast(this->ctx, std::format("@{} joined #{}", member.ctx->id(),
-                                           this->nameOrId()));
+    this->broadcast(this->ctx, std::format("@{} joined #{}", member.ctx->id(), this->nameOrId()));
   }
 
   void leave(Context *ctx) {
     members.erase(ctx->id());
-    this->broadcast(this->ctx,
-                    std::format("@{} left #{}", ctx->id(), this->nameOrId()));
+    this->broadcast(this->ctx, std::format("@{} left #{}", ctx->id(), this->nameOrId()));
   }
 
-  string nameOrId() {
-    return this->name.empty() ? this->id.string() : this->name;
-  }
+  string nameOrId() { return this->name.empty() ? this->id.string() : this->name; }
 
   void broadcast(Context *ctx, string message) {
     if (ctx != this->ctx && !this->members.contains(ctx->id())) {
@@ -139,8 +130,7 @@ public:
     }
     for (auto &member : members) {
       println(format("sending: {}", member.first));
-      member.second.ctx->send(
-          format("#{}@{}: {}", this->nameOrId(), ctx->nickOrId(), message));
+      member.second.ctx->send(format("#{}@{}: {}", this->nameOrId(), ctx->nickOrId(), message));
     }
   }
 };
@@ -185,10 +175,9 @@ enum Command {
 };
 
 static map<string, Command> commands = {
-    {"/exit", EXIT},       {"/nickname", NICKNAME}, {"/invite", INVITE},
-    {"/accept", ACCEPT},   {"/rooms", ROOMS},       {"/room", ROOM},
-    {"/message", MESSAGE}, {"/leave", LEAVE},       {"/members", MEMBERS},
-    {"/rename", RENAME},   {"/permset", PERMSET},
+    {"/exit", EXIT},       {"/nickname", NICKNAME}, {"/invite", INVITE},   {"/accept", ACCEPT},
+    {"/rooms", ROOMS},     {"/room", ROOM},         {"/message", MESSAGE}, {"/leave", LEAVE},
+    {"/members", MEMBERS}, {"/rename", RENAME},     {"/permset", PERMSET},
 };
 
 static Room GLOBAL("global");
@@ -210,139 +199,135 @@ int parse_id_or_nick(string idOrNick) {
 
 string handle_command(Context *ctx, Command command, MessageReader *reader) {
   switch (command) {
-  case EXIT:
-    ctx->close();
-    return "";
-  case NICKNAME: {
-    string nickname = trim(reader->read_to_end());
-    if (nickname.empty()) {
-      return ctx->nickname;
-    }
-    if (NICK_TO_ID.find(nickname) != NICK_TO_ID.end()) {
-      return "nickname taken";
-    }
-    NICK_TO_ID.erase(ctx->nickname);
-    ctx->nickname = nickname;
-    NICK_TO_ID.insert({ctx->nickname, ctx->id()});
-    return "Set nickname: " + nickname;
-  }
-  case ROOMS: {
-    string rooms = "Rooms:";
-    for (auto &it : ctx->rooms) {
-      auto room = it.second;
-      rooms += "\n  " + format("#{} ({})", room->name, room->id.string());
-    }
-    return rooms;
-  }
-  case ROOM: {
-    string id = trim(reader->read());
-    if (id.empty()) {
-      if (ctx->room != nullptr) {
-        return format("Current room: #{} ({})", ctx->room->name,
-                      ctx->room->id.string());
+    case EXIT:
+      ctx->close();
+      return "";
+    case NICKNAME: {
+      string nickname = trim(reader->read_to_end());
+      if (nickname.empty()) {
+        return ctx->nickname;
       }
-      return "not in room";
+      if (NICK_TO_ID.find(nickname) != NICK_TO_ID.end()) {
+        return "nickname taken";
+      }
+      NICK_TO_ID.erase(ctx->nickname);
+      ctx->nickname = nickname;
+      NICK_TO_ID.insert({ctx->nickname, ctx->id()});
+      return "Set nickname: " + nickname;
     }
-    if (id[0] != '#') {
-      return "invalid room id";
+    case ROOMS: {
+      string rooms = "Rooms:";
+      for (auto &it : ctx->rooms) {
+        auto room = it.second;
+        rooms += "\n  " + format("#{} ({})", room->name, room->id.string());
+      }
+      return rooms;
     }
-    uuid room_id = uuid(id.substr(1));
-    auto it = ctx->rooms.find(room_id);
-    if (it == ctx->rooms.end()) {
-      return "not in room";
+    case ROOM: {
+      string id = trim(reader->read());
+      if (id.empty()) {
+        if (ctx->room != nullptr) {
+          return format("Current room: #{} ({})", ctx->room->name, ctx->room->id.string());
+        }
+        return "not in room";
+      }
+      if (id[0] != '#') {
+        return "invalid room id";
+      }
+      uuid room_id = uuid(id.substr(1));
+      auto it = ctx->rooms.find(room_id);
+      if (it == ctx->rooms.end()) {
+        return "not in room";
+      }
+      auto room = it->second;
+      ctx->room = room;
+      return format("Changed default room: #{}", room->nameOrId());
     }
-    auto room = it->second;
-    ctx->room = room;
-    return format("Changed default room: #{}", room->nameOrId());
-  }
-  case RENAME: {
-    if (ctx->room->members.at(ctx->id()).permission > RoomPermission::Admin) {
-      return "insufficient permissions";
+    case RENAME: {
+      if (ctx->room->members.at(ctx->id()).permission > RoomPermission::Admin) {
+        return "insufficient permissions";
+      }
+      string name = trim(reader->read_to_end());
+      if (name.empty()) {
+        return "invalid room name";
+      }
+      ctx->room->name = name;
+      ctx->room->broadcast(ctx->room->ctx, format("room name changed to {}", name));
+      return "";
     }
-    string name = trim(reader->read_to_end());
-    if (name.empty()) {
-      return "invalid room name";
+    case PERMSET: {
+      if (ctx->room->members.at(ctx->id()).permission > RoomPermission::Admin) {
+        return "insufficient permissions";
+      }
+      int id = parse_id_or_nick(trim(reader->read()));
+      if (id < 0) {
+        return "no such user";
+      }
+      auto member = ctx->room->members.at(id);
+      member.permission = permission_from_string(reader->read());
+      ctx->room->members[id] = member;
     }
-    ctx->room->name = name;
-    ctx->room->broadcast(ctx->room->ctx,
-                         format("room name changed to {}", name));
-    return "";
-  }
-  case PERMSET: {
-    if (ctx->room->members.at(ctx->id()).permission > RoomPermission::Admin) {
-      return "insufficient permissions";
+    case INVITE: {
+      int id = parse_id_or_nick(trim(reader->read()));
+      if (id < 0) {
+        return "no such user";
+      }
+      auto recv = ACTIVE_CONTEXT.find(id);
+      if (recv == ACTIVE_CONTEXT.end()) {
+        return "no such user";
+      }
+      auto invite_ctx = recv->second;
+      Room *new_room = new Room(ctx->nickOrId() + "," + invite_ctx->nickOrId());
+      ctx->join(new_room, RoomPermission::Owner);
+      println("AFTER JOIN");
+      invite_ctx->invites.insert({new_room->id, new_room});
+      invite_ctx->send(std::format("invite from {} ({})", ctx->nickOrId(), new_room->id.string()));
+      return "invited";
     }
-    int id = parse_id_or_nick(trim(reader->read()));
-    if (id < 0) {
-      return "no such user";
+    case ACCEPT: {
+      string id = trim(reader->read());
+      auto invite = ctx->invites.find(uuid(id));
+      if (invite == ctx->invites.end()) {
+        return "no such invite";
+      }
+      auto room = invite->second;
+      cout << "Accept invite Room: " << &room << endl;
+      ctx->join(room, RoomPermission::Admin);
+      ctx->invites.erase(invite->first);
+      return "invite accepted: " + room->nameOrId();
     }
-    auto member = ctx->room->members.at(id);
-    member.permission = permission_from_string(reader->read());
-    ctx->room->members[id] = member;
-  }
-  case INVITE: {
-    int id = parse_id_or_nick(trim(reader->read()));
-    if (id < 0) {
-      return "no such user";
+    case LEAVE: {
+      string id = trim(reader->read());
+      if (id.empty() || id[0] != '#') {
+        return "invalid room id";
+      }
+      uuid room_id = uuid(id.substr(1));
+      auto it = ctx->rooms.find(room_id);
+      if (it == ctx->rooms.end()) {
+        return "not in room";
+      }
+      auto room = it->second;
+      ctx->leave(room);
+      return "left";
     }
-    auto recv = ACTIVE_CONTEXT.find(id);
-    if (recv == ACTIVE_CONTEXT.end()) {
-      return "no such user";
+    case MESSAGE: {
+      if (ctx->room == nullptr) {
+        return "not in a room";
+      }
+      string message = trim(reader->read_to_end());
+      ctx->room->broadcast(ctx, message);
+      return "sent";
     }
-    auto invite_ctx = recv->second;
-    Room *new_room = new Room(ctx->nickOrId() + "," + invite_ctx->nickOrId());
-    ctx->join(new_room, RoomPermission::Owner);
-    println("AFTER JOIN");
-    invite_ctx->invites.insert({new_room->id, new_room});
-    invite_ctx->send(std::format("invite from {} ({})", ctx->nickOrId(),
-                                 new_room->id.string()));
-    return "invited";
-  }
-  case ACCEPT: {
-    string id = trim(reader->read());
-    auto invite = ctx->invites.find(uuid(id));
-    if (invite == ctx->invites.end()) {
-      return "no such invite";
+    case MEMBERS: {
+      if (ctx->room == nullptr) {
+        return "not in a room";
+      }
+      string members = "Members: ";
+      for (auto it : ctx->room->members) {
+        members += format("\n  @{} ({})", it.second.ctx->nickOrId(), it.second.ctx->id());
+      }
+      return members;
     }
-    auto room = invite->second;
-    cout << "Accept invite Room: " << &room << endl;
-    ctx->join(room, RoomPermission::Admin);
-    ctx->invites.erase(invite->first);
-    return "invite accepted: " + room->nameOrId();
-  }
-  case LEAVE: {
-    string id = trim(reader->read());
-    if (id.empty() || id[0] != '#') {
-      return "invalid room id";
-    }
-    uuid room_id = uuid(id.substr(1));
-    auto it = ctx->rooms.find(room_id);
-    if (it == ctx->rooms.end()) {
-      return "not in room";
-    }
-    auto room = it->second;
-    ctx->leave(room);
-    return "left";
-  }
-  case MESSAGE: {
-    if (ctx->room == nullptr) {
-      return "not in a room";
-    }
-    string message = trim(reader->read_to_end());
-    ctx->room->broadcast(ctx, message);
-    return "sent";
-  }
-  case MEMBERS: {
-    if (ctx->room == nullptr) {
-      return "not in a room";
-    }
-    string members = "Members: ";
-    for (auto it : ctx->room->members) {
-      members += format("\n  @{} ({})", it.second.ctx->nickOrId(),
-                        it.second.ctx->id());
-    }
-    return members;
-  }
   }
   return "unhandled command";
 }
@@ -368,13 +353,11 @@ string dispatch_message(Context *ctx, string message) {
 
 int main(int argc, char **argv) {
   HttpService http;
-  http.GET("/",
-           [](const HttpContextPtr &ctx) { return ctx->send("hello world!"); });
+  http.GET("/", [](const HttpContextPtr &ctx) { return ctx->send("hello world!"); });
 
   WebSocketService ws;
 
-  ws.onopen = [](const WebSocketChannelPtr &channel,
-                 const HttpRequestPtr &req) {
+  ws.onopen = [](const WebSocketChannelPtr &channel, const HttpRequestPtr &req) {
     println(format("connected: @{}", channel->id()));
     Context::build(channel, &GLOBAL);
   };
